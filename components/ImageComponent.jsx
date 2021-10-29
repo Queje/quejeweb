0b8524881e53a43715dd71f4a3109a83b804f975
Queje <jeremyquerne@hotmail.fr>
2021-10-29 17:06:57 +0200

    starting my professional website project with nextjs

---
+++ components/ImageComponent.jsx
@@ -0,0 +1,12 @@
+import Image from 'next/image';
+
+const ImageComponent = (filesource) => (
+  <Image
+    src= { filesource } // Route of the image file
+    height={144} // Desired size with correct aspect ratio
+    width={144} // Desired size with correct aspect ratio
+    alt= 'Jérémy Querné, developpeur web'
+  />
+);
+
+export default ImageComponent;
