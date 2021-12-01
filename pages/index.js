146ecf12ce5afd6e5c8fdc60dfd00485153ca30e
Queje <jeremyquerne@hotmail.fr>
2021-12-01 16:59:03 +0100

    adding animation on scroll for home page subparts

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import ImageComponent from '../components/ImageComponent'
 import styles from '../styles/Home.module.css'
 import profile from '../public/images/profile.jpg'
@@ -7,8 +6,14 @@ import Navigation from '../components/Navigation'
 import Typical from 'react-typical'
 import { Col, Row } from 'react-bootstrap'
 import Wave from 'react-wavify'
+import { BsFillPhoneFill } from 'react-icons/bs'
+import { GrMail } from 'react-icons/gr'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
 
 export default function Home() {
+
+  AOS.init();
 
   return (
     <div className={styles.container}>
@@ -24,7 +29,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <Row className={styles.jumbotron}>
-          <Col md={12} className={styles.topleftcorner}>
+          <Col className={styles.topleftcorner}>
             <h1 className={styles.title}>
               <Typical
                   steps={[
@@ -46,9 +51,8 @@ export default function Home() {
             </h2>
           </Col>
         </Row>
-        
-        <Row className={styles.jumbotron2}>
-          <Col md={12} className={styles.bottomrightcorner}>
+        <Row>
+          <Col className={styles.bottomrightcorner}>
             <Wave fill='#043242'
               paused={false}
               options={{
@@ -60,8 +64,10 @@ export default function Home() {
             />
           </Col>
         </Row>
-
-        <div>
+        <div 
+          data-aos="flip-down"
+          className={styles.aboutme}
+        >
           <h2 className={styles.subtitle}>About me ...</h2>
           <Row>
             <Col md={4}>
@@ -72,30 +78,36 @@ export default function Home() {
             </Col>
           </Row>
         </div>
-
-        <div>
+        <div 
+          data-aos="flip-down"
+          className={styles.services}
+        >
           <h2 className={styles.subtitle}>Services ...</h2>
           <p className={styles.text}>Test test</p>
         </div>
-
-        <div>
+        <div 
+          data-aos="flip-down"
+          className={styles.projects}
+        >
           <h2 className={styles.subtitle}>Mes Projets</h2>
           <p className={styles.text}>test Test test</p>
         </div>
-
       </main>
-
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
+        <Row>
+          <Col md={6} className={styles.link}>
+            <a href="callto:+0678866781">
+              <BsFillPhoneFill size={36} className={styles.icons}/>
+              0678866781
+            </a>
+          </Col>
+          <Col md={6} className={styles.link}>
+            <a href="mailto:jeremy.querne@gmail.com">
+              <GrMail size={36} className={styles.icons}/>
+              email
+            </a>
+          </Col>
+        </Row>
       </footer>
     </div>
   )
