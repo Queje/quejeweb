c6e9543a729c927d0f6d20815c16107b93ab76c6
Queje <jeremyquerne@hotmail.fr>
2022-02-09 16:29:24 +0100

    mapping data to the slider with modal and tooltip

--- components/ProjectsModal.jsx
+++ components/ProjectsModal.jsx
@@ -1,26 +1,54 @@
-import { Modal } from "react-bootstrap"
+import { Modal, OverlayTrigger, Tooltip } from "react-bootstrap"
+import Image from 'next/image'
+import { useState } from "react"
+import styles from '../styles/Home.module.css'
 
-export default function ProjectsModal ({show, handleClose, title, work, details, icon1, icon2}) {
+export default function ProjectsModal ({project}) {
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
     return(
-        <Modal 
-            show={show} 
-            onHide={handleClose} 
-            centered
-        >
-            <Modal.Header closeButton closeVariant="white">
-                <Modal.Title>
-                    {title}
-                </Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                {work}
-                {details}
-            </Modal.Body>
-            <Modal.Footer>
-                {icon1}
-                {icon2}
-            </Modal.Footer>
-        </Modal>
+        <>
+            <Image
+                className={styles.SliderItem}
+                src={project.imagesource}
+                alt={project.alt} 
+                onClick={handleShow}
+            />
+            <Modal 
+                show={show} 
+                onHide={handleClose} 
+                centered
+            >
+                <Modal.Header closeButton closeVariant="white">
+                    <Modal.Title>
+                        {project.title}
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    {project.work}
+                    {project.details}
+                </Modal.Body>
+                <Modal.Footer>
+                    { project.iconlist.map((icon) =>(
+                        <OverlayTrigger
+                            key={icon.id}
+                            placement="top"
+                            delay={{ show: 250, hide: 400 }}
+                            overlay={
+                            <Tooltip className={styles.text}>
+                                {icon.tooltip}
+                            </Tooltip>
+                            }
+                        >
+                        <span>
+                            <icon.name key={icon.id} size={40}/>
+                        </span>
+                      </OverlayTrigger>
+                    ))}
+                </Modal.Footer>
+            </Modal>
+        </>
     )
 }
--- components/ProjectsSlider.jsx
+++ components/ProjectsSlider.jsx
@@ -1,13 +1,11 @@
 import { Swiper, SwiperSlide } from 'swiper/react'
 import { Navigation } from 'swiper'
-import Image from 'next/image'
 import image1 from '../public/images/nicocoaching.webp'
 import image2 from '../public/images/bretonbreaks.webp'
 import image3 from '../public/images/laurencepoidatz.webp'
 import image4 from '../public/images/salonartcarantec.webp'
 import image5 from '../public/images/learningstuff.webp'
 import styles from '../styles/Home.module.css'
-import { useState } from "react"
 import ProjectsModal from './ProjectsModal'
 import { FaCss3Alt } from 'react-icons/fa'
 import { FaWordpressSimple } from 'react-icons/fa'
@@ -15,54 +13,170 @@ import { SiJavascript } from 'react-icons/si'
 import { FaReact } from 'react-icons/fa'
 import { SiVercel } from 'react-icons/si'
 import { FaBootstrap } from 'react-icons/fa'
+import { FaWix } from 'react-icons/fa'
+import { DiRuby } from 'react-icons/di'
+import { SiRubyonrails } from 'react-icons/si'
+import { SiPostgresql } from 'react-icons/si'
+import { SiHeroku } from 'react-icons/si'
 
 export default function ProjectsSlider () {
-    const [show, setShow] = useState(false);
 
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-
-    const ProjectsList = {
-        project1 :
-            {
-                title: "nicoangercoaching.fr",
-                imagesource: image1,
-                alt: "image du site https://nicoangercoaching.fr",
-                work: "site pour un préparateur Mentale et Coach sportif",
-                details: "Construit sur Wordpress, ce travail contient une partie blog avec des articles et une inscription à une mailing list pour une news letter en utilisant MailChimp. Customisation d'un thème par le CSS.",
-                iconlist:{
-                    icon1: <FaWordpressSimple/>,
-                    icon2:  <FaCss3Alt/>,
+    const ProjectsList = [
+                {
+                    id: 1,
+                    title: "nicoangercoaching.fr",
+                    imagesource: image1,
+                    alt: "image du site https://nicoangercoaching.fr",
+                    work: "site pour un préparateur Mentale et Coach sportif",
+                    details: "Construit sur Wordpress, ce travail contient une partie blog avec des articles et une inscription à une mailing list pour une news letter en utilisant MailChimp. Customisation d'un thème par le CSS.",
+                    iconlist:[
+                        {
+                            id: 1,
+                            tooltip: "Wordpress",
+                            name: FaWordpressSimple,
+                        },
+                        {
+                            id: 2,
+                            tooltip: "css 3",
+                            name: FaCss3Alt
+                        }
+                    ]
                 },
-            },
-        project2 :
-            {
-                title: "www.bretonbreaks.com",
-                imagesource: image2,
-                alt: "image du site https://www.bretonbreaks.com",
-                work: "site pour la location d'une maison de vacances",
-                details: "Site vitrine en frontend réalisé avec Reactjs et Bootstrap, hébergé par Vercel. Site responsive (adapté pour les mobiles) fait en css customisé.",
-                iconlist:{
-                    icon1: <FaReact/>,
-                    icon2:  <SiJavascript/>,
-                    icon3: <FaBootstrap/>,
-                    icon4: <FaCss3Alt/>,
-                    icon5: <SiVercel/>
+                {
+                    id: 2,
+                    title: "www.bretonbreaks.com",
+                    imagesource: image2,
+                    alt: "image du site https://www.bretonbreaks.com",
+                    work: "site pour la location d'une maison de vacances",
+                    details: "Site vitrine en frontend réalisé avec Reactjs et Bootstrap, hébergé par Vercel. Site responsive (adapté pour les mobiles) fait en css customisé. Module de traduction français et anglais (i18next).",
+                    iconlist:[
+                        {
+                            id: 1,
+                            tooltip: "React",
+                            name: FaReact, 
+                        },
+                        {
+                            id: 2,
+                            tooltip: "Javascript",
+                            name: SiJavascript, 
+                        },
+                        {
+                            id: 3,
+                            tooltip: "Bootstrap",
+                            name: FaBootstrap,
+                        },
+                        {
+                            id: 4,
+                            tooltip: "css 3",
+                            name: FaCss3Alt,
+                        },
+                        {
+                            id: 5,
+                            tooltip: "Vercel",
+                            name: SiVercel,
+                        }
+                    ],
                 },
-            },
-        project3 : 
-            {
-
-            },
-        project4 :
-            {
-
-            },
-        project5 :
-            {
-
-            }
-    }
+                {
+                    id: 3,
+                    title: "www.laurencepoidatz.com",
+                    imagesource: image3,
+                    alt: "image du site https://www.laurencepoidatz.com",
+                    work: "site vitrine de l'artiste peintre Laurence Poidatz",
+                    details: "Site vitrine retouché sur wix.com, et retravaillé pour être responsive.",
+                    iconlist:[
+                        {
+                            id: 1,
+                            tooltip: "Wix",
+                            name: FaWix, 
+                        }
+                    ]
+                },
+                {
+                    id: 4,
+                    title: "www.salonartcarantec.com",
+                    imagesource: image4,
+                    alt: "image du site https://www.salonartcarantec.com",
+                    work: "site pour l'association du salon des arts de Carantec.",
+                    details: "Site vitrine en frontend réalisé avec Reactjs et Bootstrap, hébergé par Vercel. Site responsive (adapté pour les mobiles) fait en css customisé. Creation de formulaire imprimable en pdf sans sauvegarde des données (react-to-print). Module de traduction français et anglais (i18next). Customisation par CSS pour rendre le site responsive.",
+                    iconlist:[
+                        {
+                            id: 1,
+                            tooltip: "React",
+                            name: FaReact,
+                        },
+                        {
+                            id: 2,
+                            tooltip: "Javascript",
+                            name: SiJavascript,
+                        },
+                        {
+                            id: 3,
+                            tooltip: "Bootstrap",
+                            name: FaBootstrap,
+                        },
+                        {
+                            id: 4,
+                            tooltip: "css 3",
+                            name: FaCss3Alt,
+                        },
+                        {
+                            id: 5,
+                            tooltip: "Vercel",
+                            name: SiVercel,
+                        }
+                    ]
+                },
+                {
+                    id: 5,
+                    title: "learning-stuff",
+                    imagesource: image5,
+                    alt: "image du site https://learning-stuff-thp.herokuapp.com/",
+                    work: "site démo pour de la vente et de l'apprentissage en ligne.",
+                    details: "Site e-commerce avec le frontend réalisé avec Reactjs et Bootstrap, le backend est une api réalisé en Ruby On Rails et une base de données en postgreSQL. Ce Site est hébergé sur Heroku. Site responsive (adapté pour les mobiles) fait en css customisé. Système authentifaction avec Devise, partie administrateur avec gestion des utilisateurs. 2 types d'utilisateurs, les étudiants et les professeurs. Achats des leçons avec Stripe.",
+                    iconlist:[
+                        {
+                            id: 1,
+                            tooltip: "React",
+                            name: FaReact,
+                        },
+                        {
+                            id: 2,
+                            tooltip: "Javascript",
+                            name: SiJavascript,
+                        },
+                        {
+                            id: 3,
+                            tooltip: "Bootstrap",
+                            name: FaBootstrap,
+                        },
+                        {
+                            id: 4,
+                            tooltip: "css 3",
+                            name: FaCss3Alt, 
+                        },
+                        {
+                            id: 5,
+                            tooltip: "Ruby",
+                            name: DiRuby,    
+                        },
+                        {
+                            id: 6,
+                            tooltip: "Ruby On Rails",
+                            name: SiRubyonrails, 
+                        },
+                        {
+                            id: 7,
+                            tooltip: "PostgreSQL",
+                            name: SiPostgresql, 
+                        },
+                        {
+                            id: 8,
+                            tooltip: "Heroku",
+                            name: SiHeroku 
+                        }
+                    ]
+                }]
     
     return (
         <Swiper
@@ -74,61 +188,17 @@ export default function ProjectsSlider () {
             onSwiper={(swiper) => console.log(swiper)}
             className={styles.MySlider}
         >
+        { ProjectsList.map((project) => (
             <SwiperSlide
                 className={styles.SwipeSlider}
+                key={project.id}
             >
-                <Image
-                    className={styles.SliderItem}
-                    src={image1}
-                    alt="image du site https://nicoangercoaching.fr" 
-                    onClick={handleShow}
-                />
                 <ProjectsModal 
-                    show={show} 
-                    handleClose={handleClose} 
-                    title={ProjectsList.project1.title}
-                    work={ProjectsList.project1.work}
-                    details={ProjectsList.project1.details}
-                    icon1={ProjectsList.project1.icon1}
-                    icon2={ProjectsList.project1.icon2}
+                    key={project.id}
+                    project={project}
                 />
             </SwiperSlide>
-            <SwiperSlide>
-                <Image
-                    className={styles.SliderItem}
-                    src={image2}
-                    alt="image du site https://www.bretonbreaks.com" 
-                    onClick={handleShow}
-                />
-                < ProjectsModal show={show} handleClose={handleClose}/>
-            </SwiperSlide>
-            <SwiperSlide>
-                <Image
-                    className={styles.SliderItem}
-                    src={image3}
-                    alt="image du site https://www.laurencepoidatz.com" 
-                    onClick={handleShow}
-                />
-                < ProjectsModal show={show} handleClose={handleClose}/>
-            </SwiperSlide>
-            <SwiperSlide>
-                <Image
-                    className={styles.SliderItem}
-                    src={image4}
-                    alt="image du site https://www.salonartcarantec.com" 
-                    onClick={handleShow}
-                />
-                < ProjectsModal show={show} handleClose={handleClose}/>
-            </SwiperSlide>
-            <SwiperSlide>
-                <Image
-                    className={styles.SliderItem}
-                    src={image5}
-                    alt="image du site https://learning-stuff-thp.herokuapp.com/" 
-                    onClick={handleShow}
-                />
-                < ProjectsModal show={show} handleClose={handleClose}/>
-            </SwiperSlide>
+        ))}  
     </Swiper>
     )
 }
