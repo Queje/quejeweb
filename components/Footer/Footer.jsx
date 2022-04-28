d060395395dd949becf8574662cc1be566ee4353
Queje <jeremyquerne@hotmail.fr>
2022-04-28 17:12:40 +0200

    adding malt link and cutting typing animation time

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -6,46 +6,53 @@ import { BsGithub } from 'react-icons/bs'
 import { BsFacebook } from 'react-icons/bs'
 import { BsTwitter } from 'react-icons/bs'
 import { AiFillLinkedin } from 'react-icons/ai'
+import { IoShareSocial } from 'react-icons/io5'
 
 export default function Footer () {
 
     return(
         <footer className={styles.footer}>
             <Row className="d-flex justify-content-evenly ml-5">
-                <Col md={2} xs={12} className={styles.link}>
+                <Col md={1} xs={12} className={styles.link}>
                     <a href="tel:0033678866781">
                         <BsFillPhoneFill size={36} className={styles.icons} id="shakeanimation"/>
                         0678866781
                     </a>
                 </Col>
-                <Col md={2} xs={12} className={styles.link}>
+                <Col md={1} xs={12} className={styles.link}>
                     <a href="mailto:jeremy.querne@gmail.com">
                         <GrMail size={36} className={styles.icons} id="shakeanimation"/>
                         email
                     </a>
                 </Col>
-                <Col md={2} xs={12} className={styles.link}>
+                <Col md={1} xs={12} className={styles.link}>
                     <a href="https://github.com/Queje" target="_blank" rel="noopener noreferrer" >
                         <BsGithub size={36} className={styles.icons} id="shakeanimation"/>
                         Github
                     </a>
                 </Col>
-                <Col md={2} xs={12} className={styles.link}>
+                <Col md={1} xs={12} className={styles.link}>
                     <a href="https://www.facebook.com/profile.php?id=100079936701832" target="_blank" rel="noopener noreferrer">
                         <BsFacebook size={36} className={styles.icons} id="shakeanimation"/>
                         Facebook
                     </a>
                 </Col>
-                <Col md={2} xs={12} className={styles.link}>
+                <Col md={1} xs={12} className={styles.link}>
                     <a href="https://twitter.com/JeremyQuerne" target="_blank" rel="noopener noreferrer">
                         <BsTwitter size={36} className={styles.icons} id="shakeanimation"/>
                         Twitter
                     </a>
                 </Col>
-                <Col md={2} xs={12} className={styles.link}>
+                <Col md={1} xs={12} className={styles.link}>
                     <a href="https://www.linkedin.com/in/j%C3%A9r%C3%A9my-quern%C3%A9-01920285/" target="_blank" rel="noopener noreferrer">
                         <AiFillLinkedin size={36} className={styles.icons} id="shakeanimation"/>
                         Linkedin
+                    </a>
+                </Col>
+                <Col md={1} xs={12} className={styles.link}>
+                    <a href="https://www.malt.fr/profile/jeremyquerne?q=developpeur" target="_blank" rel="noopener noreferrer">
+                        <IoShareSocial size={36} className={styles.icons} id="shakeanimation"/>
+                        Malt
                     </a>
                 </Col>
             </Row>
